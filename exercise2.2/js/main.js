839a0dedc101621ef8d9f8bb48f1691c7c254b8f
tuyen.gabi <tuyen.gabi@gmail.com>
2019-11-18 11:50:17 +0100

    modify exercise2.2

--- exercise2.2/js/main.js
+++ exercise2.2/js/main.js
@@ -1,8 +1,8 @@
-let A = 1;
-let B = A + 3;
-A = 3;
+let A = prompt("give me a number:");
+let square = A * A;
+
 
 
 console.log('A vaut ' + A);
-console.log('B vaut ' + B);
+console.log('le carre de A est: ' + square);
 
