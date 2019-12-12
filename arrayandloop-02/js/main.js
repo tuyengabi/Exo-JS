bdfd3781670c3210a73fc507ac7686be8c5bcb9a
tuyen.gabi <tuyen.gabi@gmail.com>
2019-12-12 09:36:22 +0100

    finish arrayandloop02

--- arrayandloop-02/js/main.js
+++ arrayandloop-02/js/main.js
@@ -1,8 +1,15 @@
-let A = 1;
-let B = A + 3;
-A = 3;
+let arr = [5,10,15,17,6,18,12,7,6,2,14];
+let sum = 0;
 
+//calculer la somme des notes 
+for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+    sum += arr[i];
+}
 
-console.log('A vaut ' + A);
-console.log('B vaut ' + B);
+let average = sum / arr.length;
+
+//on peut arrondir la valeur
+average = Math.round(average);
+console.log(average);
 
