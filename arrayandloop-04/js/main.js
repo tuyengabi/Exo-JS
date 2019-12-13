38d5faf08b59e9a7e7fe153ed893a33078c7a4fe
tuyen.gabi <tuyen.gabi@gmail.com>
2019-12-13 09:28:49 +0100

    finish Arrayandloop-04

--- arrayandloop-04/js/main.js
+++ arrayandloop-04/js/main.js
@@ -1,8 +1,10 @@
-let A = 1;
-let B = A + 3;
-A = 3;
+let arr1 = [4, 8, 7, 9, 1, 5, 4, 6];
+let arr2 = [7, 6, 5, 2, 1, 3, 7, 4];
 
+let finalArr = [];
 
-console.log('A vaut ' + A);
-console.log('B vaut ' + B);
+for (let i = 0; i < arr1.length; i++) {
+    finalArr.push(arr1[i] + arr2[i]);
+}
 
+console.log(finalArr);
